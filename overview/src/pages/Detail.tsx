046c772494c252489096243e566bc6081f9555ff
Hakan ÖZER <hakanozer02@gmail.com>
2024-07-24 15:11:45 +0300

    Update Detail.tsx

--- overview/src/pages/Detail.tsx
+++ overview/src/pages/Detail.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { singleProduct } from '../services/productService'
+import { Product } from '../models/IProducts'
 
 function Detail() {
 
-  const [error, setError] = useState('')  
+  const [bigImage, setBigImage] = useState('')
+  const [error, setError] = useState('')
+  const [item, setItem] = useState<Product>()
   const params = useParams()
   useEffect(() => {
     setError('')
@@ -13,7 +16,8 @@ function Detail() {
         // detail service
         singleProduct(id).then(res => {
             const pro = res.data
-            console.log(pro)
+            setItem(pro)
+            setBigImage(pro.images[0])
         }).catch(err => {
             setError(err.response.data.message)
         })
@@ -26,6 +30,31 @@ function Detail() {
             <div className="alert alert-light text-center" role="alert">
                 {error}
             </div>
+        }
+        { item && 
+          <>
+            <div className='row'>
+              <div className='col-sm-6'>
+                <h2>{item.title}</h2>
+                <p>{item.description}</p>
+                <h2><span className="badge text-bg-danger">{item.price}₺</span></h2>
+                { item.tags.map((tag, index ) => 
+                  <span key={index} className="badge text-bg-secondary" style={{marginRight: 3,}}>{tag}</span>
+                )}
+                <div className='mt-3'>
+                  <img src={item.meta.qrCode} width={150} className='img-thumbnail' />
+                </div>
+              </div>
+              <div className='col-sm-6'>
+                <img src={bigImage} className='img-fluid' style={{height: 450,}}  />
+                <div className='mt-3'>
+                    {item.images.map((url, index) => 
+                      <img key={index} onClick={(evt) => setBigImage(url)} src={url} className='img-thumbnail' style={{height: 125, marginRight: 3,}} />
+                    )}
+                </div>
+              </div>
+            </div>
+          </>
         }
     </>
   )
